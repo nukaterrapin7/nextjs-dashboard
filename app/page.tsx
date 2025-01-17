608638a27994f9678e1de97023b0ae4fb7d57db6
scott <pythonism007@gmail.com>
2025-01-17 11:47:28 -0600

    Completes chapters 15 and 16, and completes tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import Link from 'next/link';
 import { Lusitana } from 'next/font/google';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
